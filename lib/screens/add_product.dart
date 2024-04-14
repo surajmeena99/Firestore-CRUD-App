d9a9327a700d92c56b9bd777d7a994ceec376949
surajmeena99 <surajmeena.dev@gmail.com>
2024-04-14 19:33:04 +0530

    add form

--- lib/screens/add_product.dart
+++ lib/screens/add_product.dart
@@ -11,12 +11,10 @@ class AddProduct extends StatefulWidget {
 class _AddProductState extends State<AddProduct> {
 
   final DatabaseService _databaseService = DatabaseService();
+  final formKey = GlobalKey<FormState>();
 
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _priceController = TextEditingController();
-
-  bool _validateName = false;
-  bool _validatePrice = false;
 
   @override
   Widget build(BuildContext context) {
@@ -27,77 +25,82 @@ class _AddProductState extends State<AddProduct> {
       body: SingleChildScrollView(
         child: Container(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text('Add New Product',
-                style: TextStyle(fontSize: 20, color: Colors.teal, fontWeight: FontWeight.w500),
-              ),
-              const SizedBox(height: 20.0,),
-              TextField(
+          child: Form(
+            key: formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('Add New Product',
+                  style: TextStyle(fontSize: 20, color: Colors.teal, fontWeight: FontWeight.w500),
+                ),
+                const SizedBox(height: 20.0,),
+                TextFormField(
                   controller: _nameController,
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Name Value Can\'t Be Empty';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
                     hintText: 'Enter Name',
                     labelText: 'Name',
-                    errorText: _validateName ? 'Name Value Can\'t Be Empty' : null,
-                  )),
-              const SizedBox(height: 20.0,),
-              TextField(
+                  )
+                ),
+                const SizedBox(height: 20.0,),
+                TextFormField(
                   controller: _priceController,
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    hintText: 'Enter Contact',
-                    labelText: 'Contact',
-                    errorText: _validatePrice ? 'Price Value Can\'t Be Empty' : null,
-                  )),
-              const SizedBox(height: 20.0,),
-              Row(
-                children: [
-                  TextButton(
-                      style: TextButton.styleFrom(
-                          foregroundColor: Colors.white,
-                          backgroundColor: Colors.teal,
-                          textStyle: const TextStyle(fontSize: 15),
-                      ),
-                      onPressed: () async {
-                        setState(() {
-                          _nameController.text.isEmpty
-                              ? _validateName = true
-                              : _validateName = false;
-                          _priceController.text.isEmpty
-                              ? _validatePrice = true
-                              : _validatePrice = false;
-                        });
-                        Map<String, dynamic> data = {
-                          "name" : _nameController.text,
-                          "price" : double.tryParse(_priceController.text)!,
-                        };
-                        if (_validateName == false &&  _validatePrice == false) {
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Price Value Can\'t Be Empty';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter Price',
+                    labelText: 'Price',
+                  )
+                ),
+                const SizedBox(height: 20.0,),
+                Row(
+                  children: [
+                    TextButton(
+                        style: TextButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.teal,
+                            textStyle: const TextStyle(fontSize: 15),
+                        ),
+                        onPressed: () async {
+                          if (formKey.currentState!.validate()) {
+                            Map<String, dynamic> data = {
+                              "name" : _nameController.text,
+                              "price" : double.tryParse(_priceController.text)!,
+                            };
                             await _databaseService.addData(data);
-
-                            _nameController.text = '';
-                            _priceController.text = '';
                             Navigator.of(context).pop();
-                        }
-                      },
-                      child: const Text('Add Product')),
-                  const SizedBox(
-                    width: 10.0,
-                  ),
-                  TextButton(
-                      style: TextButton.styleFrom(
-                          foregroundColor: Colors.white,
-                          backgroundColor: Colors.red,
-                          textStyle: const TextStyle(fontSize: 15)),
-                      onPressed: () {
-                        _nameController.text = '';
-                        _priceController.text = '';
-                      },
-                      child: const Text('Clear Data'))
-                ],
-              )
-            ],
+                          }
+                          // Don't write here Navigator.of(context).pop();
+                        },
+                        child: const Text('Add Product')),
+                    const SizedBox(
+                      width: 10.0,
+                    ),
+                    TextButton(
+                        style: TextButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.red,
+                            textStyle: const TextStyle(fontSize: 15)),
+                        onPressed: () {
+                          _nameController.text = '';
+                          _priceController.text = '';
+                        },
+                        child: const Text('Clear Data'))
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
--- lib/screens/edit_product.dart
+++ lib/screens/edit_product.dart
@@ -14,12 +14,9 @@ class EditProduct extends StatefulWidget {
 class _EditProductState extends State<EditProduct> {
 
   final DatabaseService _databaseService = DatabaseService();
-
+  final formKey = GlobalKey<FormState>();
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _priceController = TextEditingController();
-
-  bool _validateName = false;
-  bool _validatePrice = false;
 
   @override
   initState() {
@@ -32,80 +29,85 @@ class _EditProductState extends State<EditProduct> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Edit Product"),
+        title: const Text("Edit Product"),
       ),
       body: SingleChildScrollView(
         child: Container(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text('Edit This Product',
-                style: TextStyle(fontSize: 20, color: Colors.teal, fontWeight: FontWeight.w500),
-              ),
-              const SizedBox(height: 20.0,),
-              TextField(
-                  controller: _nameController,
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    hintText: 'Enter Name',
-                    labelText: 'Name',
-                    errorText: _validateName ? 'Name Value Can\'t Be Empty' : null,
-                  )),
-              const SizedBox(height: 20.0,),
-              TextField(
-                  controller: _priceController,
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    hintText: 'Enter Contact',
-                    labelText: 'Contact',
-                    errorText: _validatePrice ? 'Price Value Can\'t Be Empty' : null,
-                  )),
-              const SizedBox(height: 20.0,),
-              Row(
-                children: [
-                  TextButton(
-                      style: TextButton.styleFrom(
-                          foregroundColor: Colors.white,
-                          backgroundColor: Colors.teal,
-                          textStyle: const TextStyle(fontSize: 15),
-                      ),
-                      onPressed: () async {
-                        setState(() {
-                          _nameController.text.isEmpty
-                              ? _validateName = true
-                              : _validateName = false;
-                          _priceController.text.isEmpty
-                              ? _validatePrice = true
-                              : _validatePrice = false;
-                        });
-                        final String name = _nameController.text;
-                        final double? price = double.tryParse(_priceController.text);
-                        if (_validateName == false &&  _validatePrice == false) {
-                          await _databaseService.updateData(widget.editProduct.id, name, price!);
+          child: Form(
+            key: formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('Edit This Product',
+                  style: TextStyle(fontSize: 20, color: Colors.teal, fontWeight: FontWeight.w500),
+                ),
+                const SizedBox(height: 20.0,),
+                TextFormField(
+                    controller: _nameController,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Name Value Can\'t Be Empty';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter Name',
+                      labelText: 'Name',
+                    )),
+                const SizedBox(height: 20.0,),
+                TextFormField(
+                    controller: _priceController,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Price Value Can\'t Be Empty';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter Price',
+                      labelText: 'Price',
+                    )),
+                const SizedBox(height: 20.0,),
+                Row(
+                  children: [
+                    TextButton(
+                        style: TextButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.teal,
+                            textStyle: const TextStyle(fontSize: 15),
+                        ),
+                        onPressed: () async {
+                          if (formKey.currentState!.validate()) {
+                            final String name = _nameController.text;
+                            final double? price = double.tryParse(_priceController.text);
 
+                            await _databaseService.updateData(widget.editProduct.id, name, price!);
+                            Navigator.of(context).pop();
+                          }
+                          // Don't write here Navigator.of(context).pop();
+                          
+                        },
+                        child: const Text('Update Product')),
+                    const SizedBox(
+                      width: 10.0,
+                    ),
+                    TextButton(
+                        style: TextButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.red,
+                            textStyle: const TextStyle(fontSize: 15)),
+                        onPressed: () {
                           _nameController.text = '';
                           _priceController.text = '';
-                          Navigator.of(context).pop();
-                        }
-                      },
-                      child: const Text('Update Product')),
-                  const SizedBox(
-                    width: 10.0,
-                  ),
-                  TextButton(
-                      style: TextButton.styleFrom(
-                          foregroundColor: Colors.white,
-                          backgroundColor: Colors.red,
-                          textStyle: const TextStyle(fontSize: 15)),
-                      onPressed: () {
-                        _nameController.text = '';
-                        _priceController.text = '';
-                      },
-                      child: const Text('Clear Data'))
-                ],
-              )
-            ],
+                        },
+                        child: const Text('Clear Data'))
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
